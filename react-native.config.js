b132e5d2c52acdf4d16eab35199b4d6af0fb43e3
minjung <k1950035@naver.com>
2024-05-09 03:26:31 +0900

    Feat: Add tailwind config
    tailwind.config.js에 커스텀 토큰을 추가했습니다. 또한, assets/font에
    프로젝트에 사용될 폰트들을 추가했으며 이를 이어주는 역할을 수행하는
    react-native.config.js 파일 또한 생성했습니다.

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/'], // font 파일이 위치한 경로.
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,34 @@ module.exports = {
     './components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-1': '#F0F0F0',
+        'gray-2': '#C2C2C2',
+        'gray-3': '#D9D9D9',
+        'primary-1': '#C6F3F3',
+        'primary-2': '#39C3C5',
+        point: '#F19B22',
+        failed: '#FF0000',
+        'mbti-green': '#33A373',
+        'mbti-blue': '#00F4FF',
+        'mbti-yellow': '#E4C728',
+        'mbti-pink': '#C0226D',
+      },
+      fontFamily: {
+        'inter-sb': ['Inter-SemiBold'],
+        'inter-m': ['Inter-Medium'],
+        'inter-b': ['Inter-Bold'],
+        'inter-r': ['Inter-Regular'],
+      },
+      fontSize: {
+        'hs-s': '14px',
+        'hs-m': '16px',
+        'hs-l': '18px',
+        'hs-xl': '20px',
+        'hs-2xl': '25px',
+      },
+    },
   },
   plugins: [],
 };
