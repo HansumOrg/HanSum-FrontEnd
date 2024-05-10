aa7ebb5f6f4be9ee9bc20ce5a1b85c48593d202b
minjung <k1950035@naver.com>
2024-05-10 22:16:35 +0900

    Feat: Choice
    mypage의 choice들을 구현했습니다.

--- screens/main/my-page/MyPageScreen.tsx
+++ screens/main/my-page/MyPageScreen.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, View, Text, Pressable } from 'react-native';
+import {
+  SafeAreaView,
+  StatusBar,
+  View,
+  Text,
+  Pressable,
+  ScrollView,
+} from 'react-native';
 import { MyPageStackScreenProps } from '../../../navigation/types';
 import NoticeIcon from '../../../assets/images/icon_notice.svg';
 import MoreIcon from '../../../assets/images/icon_more.svg';
+import { Shadow } from 'react-native-shadow-2'
 
 export default function MyPageScreen({
   // route와 navigation 사용 안할 시 제거해주세요.
@@ -14,18 +22,18 @@ export default function MyPageScreen({
   return (
     <SafeAreaView>
       <StatusBar barStyle="default" />
-      <View className="h-screen w-screen flex items-center">
+      <View className="h-screen w-screen flex items-center bg-white">
         <View className="flex flex-row w-11/12 h-auto justify-between items-center py-2">
-          <Text className="font-inter-b w-5/6 text-2xl text-black">
+          <Text className="font-inter-b w-5/6 text-xl text-black">
             마이페이지
           </Text>
           <Pressable>
             <NoticeIcon width={29} height={29} />
           </Pressable>
         </View>
-        <View className="flex flex-col w-11/12 h-2/6 ">
-          <View className="flex flex-row w-auto h-1/5 justify-between items-center">
-            <View className="flex flex-col h-full">
+        <View className="flex flex-col w-11/12 h-1/3">
+          <View className="flex flex-row w-auto h-1/4 justify-between items-end">
+            <View className="flex flex-col h-full justify-end">
               <Text className="font-inter-b text-md text-black">이한이</Text>
               <Text className="font-inter-m text-sm text-black/[.5]">
                 Show profile
@@ -36,14 +44,44 @@ export default function MyPageScreen({
             </Pressable>
           </View>
           <View className="flex border-b" />
-          <View className="flex flex-col w-full h-full bg-green-500">
-            <Text className="font-inter-m text-md">고객센터</Text>
-            <Text className="font-inter-m text-md">문의사항</Text>
-            <Text className="font-inter-m text-md">공지사항</Text>
-            <Text className="font-inter-m text-md">로그아웃</Text>
+          <View className="flex flex-col w-full h-full">
+            <View className="flex flex-row w-full h-1/5 justify-between items-end">
+              <Text className="font-inter-m text-md text-black">고객센터</Text>
+              <Pressable className="px-1">
+                <MoreIcon width={29} height={29} />
+              </Pressable>
+            </View>
+            <View className="flex border-b" />
+            <View className="flex flex-row w-full h-1/6 justify-between items-end">
+              <Text className="font-inter-m text-md text-black">문의사항</Text>
+              <Pressable className="px-1">
+                <MoreIcon width={29} height={29} />
+              </Pressable>
+            </View>
+            <View className="flex border-b" />
+            <View className="flex flex-row w-full h-1/6 justify-between items-end">
+              <Text className="font-inter-m text-md text-black">공지사항</Text>
+              <Pressable className="px-1">
+                <MoreIcon width={29} height={29} />
+              </Pressable>
+            </View>
+            <View className="flex border-b" />
+            <View className="flex flex-row w-full h-1/6 justify-between items-end">
+              <Text className="font-inter-m text-md text-black">로그아웃</Text>
+              <Pressable className="px-1">
+                <MoreIcon width={29} height={29} />
+              </Pressable>
+            </View>
+            <View className="flex border-b" />
           </View>
         </View>
-        <View className="flex flex-row bg-blue-300 w-11/12 h-3/6"></View>
+        <View className="flex flex-col w-11/12 h-3/6">
+          <View className="flex h-1/6 justify-end">
+            <Text className="font-inter-b text-lg text-black">내 예약</Text>
+          </View>
+          <ScrollView className="flex flex-colw-full h-full">
+          </ScrollView>
+        </View>
       </View>
     </SafeAreaView>
   );
