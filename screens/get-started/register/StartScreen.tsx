8f37ddc0b981d586c4c5082460c0c156dfd74cf3
leeji0805 <bomnaeum104@naver.com>
2024-05-14 01:35:31 +0900

    Design: px 제거

--- screens/get-started/register/StartScreen.tsx
+++ screens/get-started/register/StartScreen.tsx
@@ -12,7 +12,7 @@ export default function StartScreen({
       <StatusBar barStyle="default" />
       <View className="h-screen px-6 pt-6 bg-white">
         <View className=" h-4/5 bg-white items-center justify-center ">
-          <View className="h-36 bg-gray-2 w-36" />
+          <View className="h-1/4 bg-gray-2 w-2/4" />
           <View className=" bg-white items-center mt-6">
             <Text>“한 숨”을 통해</Text>
             <Text> 새로운 사람들과 소중한 만남을 시작해보세요!!</Text>
