6b94122ce96ab4a5836ff3cd61f43e54c03be637
minjung <k1950035@naver.com>
2024-05-16 07:11:16 +0900

    Refactor: mypage conponentize
    MyPageScreen의 ReservationBox를 컴포넌트화 시켜 리팩토링 하였습니다.

--- screens/main/my-page/MyPageScreen.tsx
+++ screens/main/my-page/MyPageScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -10,135 +10,9 @@ import {
 import { MyPageStackScreenProps } from '../../../navigation/types';
 import NoticeIcon from '../../../assets/images/icon_notice.svg';
 import MoreIcon from '../../../assets/images/icon_more.svg';
-import LocalIcon from '../../../assets/images/icon_local.svg';
-import PhoneIcon from '../../../assets/images/icon_phone.svg';
 import data from '../../../data.json';
-
-interface Reservation {
-  reservation_id: number;
-  user_id: number;
-  guesthouse_id: number;
-  checkin_date: string;
-  checkout_date: string;
-}
-
-interface User {
-  user_id: number;
-  login_id: string;
-  password: string;
-  username: string;
-  phone: string;
-  sex: string;
-  birthday: string;
-  nickname: string;
-  mbti: string;
-  user_agreement: number;
-  interested_location: string[];
-  interest_hobby: string[];
-  interested_food: string[];
-}
-
-interface Guesthouse {
-  guesthouse_id: number;
-  guesthouse_name: string;
-  address: string;
-  location: string;
-  price: number;
-  phone: string;
-  rating: number;
-  imageUrl: string;
-  mood: string;
-}
-
-interface ReservationBoxProps {
-  reservation: Reservation;
-  guesthouse: Guesthouse;
-}
-
-function DateCheck({ checkin_date }: Reservation): number {
-  const today = new Date();
-  const checkin = new Date(checkin_date);
-  let status = 0;
-  if (today < checkin) {
-    status = 0;
-  } else {
-    status = 1;
-  }
-  return status;
-}
-function ReservationBox(props: ReservationBoxProps) {
-  const { reservation, guesthouse } = props;
-  const [dateState, setDateState] = useState(0); // 0: 예약확정, 1:이용완료
-  useEffect(() => {
-    setDateState(DateCheck(reservation));
-  }, [reservation]);
-  return (
-    <View className="flex bg-white h-auto mb-1 w-full items-center">
-      <View className="flex flex-row m-2 w-11/12 h-auto rounded-xl shadow-md shadow-black/50 bg-pink-500">
-        <View className="flex flex-row h-auto w-full">
-          <View className="flex flex-col w-4/5 h-auto rounded-l-md bg-white">
-            <Text className="font-inter-m mt-2 mb-1 ml-2 text-xl text-black">
-              {guesthouse.guesthouse_name}
-            </Text>
-            <View className="flex flex-row mx-1 px-1 w-full h-auto">
-              <LocalIcon width={12.8} height={16} />
-              <Text className="font-inter-r mx-1 text-sm  text-black">
-                {guesthouse.address}
-              </Text>
-            </View>
-            <View className="flex flex-row m-1 px-1 w-full h-auto">
-              <PhoneIcon width={16} height={16} />
-              <Text className="font-inter-r mx-1 text-sm  text-black">
-                {guesthouse.phone.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3')}
-              </Text>
-            </View>
-            {dateState === 0 ? (
-              <View className="flex flex-col w-3/5 h-auto">
-                <View className="flex px-2 flex-row w-full justify-between">
-                  <Text className="font-inter-m text-sm text-black/[.50]">
-                    Check-in
-                  </Text>
-                  <Text className="font-inter-m text-sm text-black/[.50]">
-                    {reservation.checkin_date.split(' ')[1]}
-                  </Text>
-                </View>
-                <View className="flex px-2 mb-2 flex-row w-full justify-between">
-                  <Text className="font-inter-m text-sm text-black/[.50]">
-                    Check-out
-                  </Text>
-                  <Text className="font-inter-m text-sm text-black/[.50]">
-                    {reservation.checkout_date.split(' ')[1]}
-                  </Text>
-                </View>
-              </View>
-            ) : (
-              <Pressable className="flex flex-col w-full h-auto justify-center items-center">
-                <View className="flex bg-gray-3 w-11/12 h-auto items-center m-2 rounded-lg">
-                  <Text className="font-inter-m text-lg m-2 text-black">
-                    후기 작성
-                  </Text>
-                </View>
-              </Pressable>
-            )}
-          </View>
-          {dateState === 0 ? (
-            <View className="flex w-1/5 h-auto rounded-r-md bg-primary-2 shadow-md shadow-black/40 items-center justify-center">
-              <Text className="font-inter-sb text-lg text-white">
-                예약{'\n'}확정
-              </Text>
-            </View>
-          ) : (
-            <View className="flex w-1/5 h-auto rounded-r-md bg-point shadow-md shadow-black/40 items-center justify-center">
-              <Text className="font-inter-sb text-lg text-white">
-                이용{'\n'}완료
-              </Text>
-            </View>
-          )}
-        </View>
-      </View>
-    </View>
-  );
-}
+import { User, Reservation, Guesthouse } from '../../../types';
+import ReservationBox from '../../../components/my-page/ReservationBox';
 
 export default function MyPageScreen({
   // route와 navigation 사용 안할 시 제거해주세요.
--- types.ts
+++ types.ts
@@ -1,1 +1,44 @@
 // this file contains all the types that are used in the application except for navigation types
+
+// my-page types
+
+export interface Reservation {
+  reservation_id: number;
+  user_id: number;
+  guesthouse_id: number;
+  checkin_date: string;
+  checkout_date: string;
+}
+
+export interface User {
+  user_id: number;
+  login_id: string;
+  password: string;
+  username: string;
+  phone: string;
+  sex: string;
+  birthday: string;
+  nickname: string;
+  mbti: string;
+  user_agreement: number;
+  interested_location: string[];
+  interest_hobby: string[];
+  interested_food: string[];
+}
+
+export interface Guesthouse {
+  guesthouse_id: number;
+  guesthouse_name: string;
+  address: string;
+  location: string;
+  price: number;
+  phone: string;
+  rating: number;
+  imageUrl: string;
+  mood: string;
+}
+
+export interface ReservationBoxProps {
+  reservation: Reservation;
+  guesthouse: Guesthouse;
+}
