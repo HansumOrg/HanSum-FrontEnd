d84121f43eb4525c0bf63c4d48285492d3627684
minjung <k1950035@naver.com>
2024-05-19 02:15:10 +0900

    Feat: SearchScreen
    SearchScreen의 디자인을 구현하였습니다.

--- screens/main/search/SearchScreen.tsx
+++ screens/main/search/SearchScreen.tsx
@@ -3,6 +3,7 @@ import { SafeAreaView, StatusBar, View, Text, TextInput } from 'react-native';
 import { SearchStackScreenProps } from '../../../navigation/types';
 import SearchIcon from '../../../assets/images/icon_search.svg';
 import CalendarIcon from '../../../assets/images/icon_calendar.svg';
+import MoreIcon from '../../../assets/images/icon_goback.svg';
 
 export default function SearchScreen({
   // route와 navigation 사용 안할 시 제거해주세요.
@@ -17,14 +18,14 @@ export default function SearchScreen({
       <View className="h-screen bg-white flex justify-center items-center">
         <View className="flex w-11/12 h-full">
           <View className="flex w-full h-1/4 justify-end">
-            <View className="flex w-full h-4/6  items-center justify-center">
+            <View className="flex w-full h-4/5  items-center justify-end ">
               <View className="flex flex-row w-full h-1/3 mb-1 bg-white border-2 border-gray-1/100 rounded-lg">
                 <View className="flex ml-2 w-auto h-full justify-center">
                   <SearchIcon width={26} height={27} />
                 </View>
                 <View className="flex w-3/4 h-full mt-1 justify-center">
                   <TextInput
-                    className="font-inter-m text-lg text-gray-1"
+                    className="font-inter-m text-sm text-black/100"
                     placeholder="지역, 게스트하우스 이름"
                   />
                 </View>
@@ -35,21 +36,74 @@ export default function SearchScreen({
                 </View>
                 <View className="flex w-3/4 h-full mt-1 justify-center">
                   <TextInput
-                    className="font-inter-m text-lg text-gray-1"
+                    className="font-inter-m text-sm text-black/100"
                     placeholder="날짜 선택"
                   />
                 </View>
               </View>
             </View>
-            <View className="flex w-full h-1/6">
-              <Text className="font-inter-b text-xl text-black">
+            <View className="flex w-full h-1/5 justify-end">
+              <Text className="font-inter-b text-lg text-black">
                 제주도 여행지
               </Text>
             </View>
           </View>
-          <View className="flex w-full h-full bg-slate-300">
-            <View className="flex w-full h-3/6 bg-blue-200">
-              <View className="flex w-full h-1/6 bg-pink-300"></View>
+          <View className="flex w-full h-full my-4">
+            <View className="flex w-full h-1/2">
+              <View className="flex flex-col w-full h-auto py-3">
+                <View className="flex flex-row w-full h-auto justify-between">
+                  <Text className="font-inter-r inter-sm text-black">
+                    제주공항 서부(용담, 도두, 연동, 노형동)
+                  </Text>
+                  <MoreIcon width={23} height={22} />
+                </View>
+                <View className="flex w-full py-1 border-b border-gray-2" />
+              </View>
+              <View className="flex flex-col w-full h-auto py-3">
+                <View className="flex flex-row w-full h-auto justify-between">
+                  <Text className="font-inter-r inter-sm text-black">
+                    제주공항 동부(제주시청, 탑동, 건입동)/추자도
+                  </Text>
+                  <MoreIcon width={23} height={22} />
+                </View>
+                <View className="flex w-full py-1 border-b border-gray-2" />
+              </View>
+              <View className="flex flex-col w-full h-auto py-3">
+                <View className="flex flex-row w-full h-auto justify-between">
+                  <Text className="font-inter-r inter-sm text-black">
+                    서귀포시/중문/모슬포
+                  </Text>
+                  <MoreIcon width={23} height={22} />
+                </View>
+                <View className="flex w-full py-1 border-b border-gray-2" />
+              </View>
+              <View className="flex flex-col w-full h-auto py-3">
+                <View className="flex flex-row w-full h-auto justify-between">
+                  <Text className="font-inter-r inter-sm text-black">
+                    이호테우/하귀/애월/한림/협재
+                  </Text>
+                  <MoreIcon width={23} height={22} />
+                </View>
+                <View className="flex w-full py-1 border-b border-gray-2" />
+              </View>
+              <View className="flex flex-col w-full h-auto py-3">
+                <View className="flex flex-row w-full h-auto justify-between">
+                  <Text className="font-inter-r inter-sm text-black">
+                    함덕/김녕/세화
+                  </Text>
+                  <MoreIcon width={23} height={22} />
+                </View>
+                <View className="flex w-full py-1 border-b border-gray-2" />
+              </View>
+              <View className="flex flex-col w-full h-auto py-3">
+                <View className="flex flex-row w-full h-auto justify-between">
+                  <Text className="font-inter-r inter-sm text-black">
+                    남원/표선/성산
+                  </Text>
+                  <MoreIcon width={23} height={22} />
+                </View>
+                <View className="flex w-full py-1 border-b border-gray-2" />
+              </View>
             </View>
           </View>
         </View>
