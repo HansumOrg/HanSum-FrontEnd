510023ca74b0bbf27710b5416d7cd7f803c5de00
minjung <k1950035@naver.com>
2024-05-21 03:02:57 +0900

    Feat: Select days
    예약하고 싶은 날짜의 범위를 선택할 수 있는 기능을 구현했습니다.

--- components/search-page/Calendar.tsx
+++ components/search-page/Calendar.tsx
@@ -1,6 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { Pressable, Text, View } from 'react-native';
 
+function OnPressFunction( // 예약 범위 설정
+  currentDate: Date,
+  today: Date,
+  reservationStartDate: Date | null,
+  reservationEndDate: Date | null,
+  setReservationStartDate: (date: Date | null) => void,
+  setReservationEndDate: (date: Date | null) => void,
+) {
+  if (!reservationStartDate && currentDate >= today) {
+    // 예약 시작 날짜가 없고, 현재 날짜가 오늘 이후일 때
+    setReservationStartDate(currentDate);
+  }
+  if (reservationStartDate && !reservationEndDate && currentDate >= today) {
+    // 예약 시작 날짜가 있고, 예약 종료 날짜가 없고, 현재 날짜가 오늘 이후일 때
+    setReservationEndDate(currentDate);
+  }
+  if (reservationStartDate && currentDate < today) {
+    // 예약 시작 날짜가 있고, 현재 날짜가 오늘 이전일 때
+    setReservationStartDate(null);
+    setReservationEndDate(null);
+  }
+  if (reservationStartDate && reservationEndDate && currentDate >= today) {
+    // 예약 시작 날짜와 예약 종료 날짜가 있고, 현재 날짜가 오늘 이후일 때
+    setReservationEndDate(currentDate);
+  }
+  if (
+    reservationStartDate &&
+    reservationEndDate &&
+    currentDate < reservationStartDate
+  ) {
+    // 예약 시작 날짜와 예약 종료 날짜가 있고, 현재 날짜가 예약 시작일 이전일 때
+    setReservationStartDate(null);
+    setReservationEndDate(null);
+  }
+}
+
 function GetDayOfWeek(
   date: Date,
   reservationStartDate: Date | null,
@@ -17,6 +53,7 @@ function GetDayOfWeek(
   const weeks = [];
   let week = [];
   for (let i = 0; i < firstDayOfWeek; i += 1) {
+    // 첫째 주 시작 전까지 빈 칸 채우기
     week.push(<View className="flex w-[14%]" />);
   }
   const today = new Date();
@@ -29,46 +66,44 @@ function GetDayOfWeek(
     const isReserved =
       reservationStartDate &&
       reservationEndDate &&
-      currentDate >= reservationStartDate &&
-      currentDate <= reservationEndDate;
-    const isReservedBorder = isReserved ? 'bg-primary-1/[.50]' : '';
-    const isTodayColor = isToday ? 'bg-primary-1 rounded-full' : 'bg-white';
+      currentDate > reservationStartDate &&
+      currentDate < reservationEndDate;
+    const isReservedText = isReserved ? 'text-primary-2' : 'text-black';
+    const isTodayColor = isToday ? 'border-primary-2 border rounded-full' : '';
+    const isReservationStart =
+      reservationStartDate &&
+      currentDate.getTime() === reservationStartDate.getTime();
+    const isReservationStartColor = isReservationStart
+      ? 'bg-primary-2/[.50] rounded-full'
+      : '';
+    const isReservationEnd =
+      reservationEndDate &&
+      currentDate.getTime() === reservationEndDate.getTime();
+    const isReservationEndColor = isReservationEnd
+      ? 'bg-primary-2/[.50] rounded-full'
+      : '';
     if (i < 10) {
       week.push(
         <Pressable
           key={i}
           className="flex w-[14%] flex-row items-center justify-center"
-          onPress={() => {
-            if (!reservationStartDate && currentDate >= today) {
-              setReservationStartDate(currentDate);
-            }
-            if (
-              reservationStartDate &&
-              !reservationEndDate &&
-              currentDate >= today
-            ) {
-              setReservationEndDate(currentDate);
-            }
-            if (
-              reservationStartDate &&
-              reservationEndDate &&
-              currentDate < reservationStartDate
-            ) {
-              setReservationStartDate(currentDate);
-            }
-            if (
-              reservationStartDate &&
-              reservationEndDate &&
-              currentDate > reservationEndDate
-            ) {
-              setReservationEndDate(currentDate);
-            }
-          }}
+          onPress={() =>
+            OnPressFunction(
+              currentDate,
+              today,
+              reservationStartDate,
+              reservationEndDate,
+              setReservationStartDate,
+              setReservationEndDate,
+            )
+          }
         >
           <View
-            className={`flex w-auto h-full ${isTodayColor} ${isReservedBorder} py-3 px-4`}
+            className={`flex w-auto h-full ${isTodayColor} ${isReservationStartColor} ${isReservationEndColor} py-3 px-4`}
           >
-            <Text className="font-inter-r text-sm text-black">{i}</Text>
+            <Text className={`font-inter-r text-sm ${isReservedText}`}>
+              {i}
+            </Text>
           </View>
         </Pressable>,
       );
@@ -77,33 +112,23 @@ function GetDayOfWeek(
         <Pressable
           key={i}
           className="flex w-[14%] flex-row items-center justify-center"
-          onPress={() => {
-            if (!reservationStartDate) {
-              setReservationStartDate(currentDate);
-            }
-            if (reservationStartDate && !reservationEndDate) {
-              setReservationEndDate(currentDate);
-            }
-            if (
-              reservationStartDate &&
-              reservationEndDate &&
-              currentDate < reservationStartDate
-            ) {
-              setReservationStartDate(currentDate);
-            }
-            if (
-              reservationStartDate &&
-              reservationEndDate &&
-              currentDate > reservationEndDate
-            ) {
-              setReservationEndDate(currentDate);
-            }
-          }}
+          onPress={() =>
+            OnPressFunction(
+              currentDate,
+              today,
+              reservationStartDate,
+              reservationEndDate,
+              setReservationStartDate,
+              setReservationEndDate,
+            )
+          }
         >
           <View
-            className={`flex w-auto h-full ${isTodayColor} ${isReservedBorder} p-3`}
+            className={`flex w-auto h-full ${isTodayColor} ${isReservationStartColor} ${isReservationEndColor}  p-3`}
           >
-            <Text className="font-inter-r text-sm text-black">{i}</Text>
+            <Text className={`font-inter-r text-sm ${isReservedText}`}>
+              {i}
+            </Text>
           </View>
         </Pressable>,
       );
@@ -130,15 +155,6 @@ export default function Calendar() {
   const [reservationEndDate, setReservationEndDate] = useState<Date | null>(
     null,
   ); // 예약 종료 날짜
-
-  useEffect(() => {
-    console.log('start', reservationStartDate);
-  }, [reservationStartDate]);
-
-  useEffect(() => {
-    console.log('end', reservationEndDate);
-  }, [reservationEndDate]);
-
   const renderMonths = () => {
     const months = [];
     for (let i = 0; i < 6; i += 1) {
