aebbc969bf5cb3df65bc43a27c55b7e03a9e7cd3
minjung <k1950035@naver.com>
2024-05-26 04:41:39 +0900

    Feat: Converting Svg
    svg 파일의 형식을 변경하여 클릭했을 때의 모든 스타일이 변경되도록 코드를 구현했습니다.

--- screens/main/search/FilterScreen.tsx
+++ screens/main/search/FilterScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StatusBar, View, Text, Pressable } from 'react-native';
 import { SearchStackScreenProps } from '../../../navigation/types';
 import CheckIcon from '../../../assets/images/icon_checkbox.svg';
@@ -16,42 +16,98 @@ export default function FilterScreen({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   navigation,
 }: SearchStackScreenProps<'Filter'>) {
+  // 분위기 필터
+  const [activity, setActivity] = useState(false);
+  const [healing, setHealing] = useState(false);
+  const [energetic, setEnergetic] = useState(false);
+  const [leisure, setLeisure] = useState(false);
+  // 시설 서비스 필터
+  const [party, setParty] = useState(false);
+  const [breakfast, setBreakfast] = useState(false);
+  const [singleRoom, setSingleRoom] = useState(false);
+  const [parking, setParking] = useState(false);
+  const [swimming, setSwimming] = useState(false);
+  const [womenOnly, setWomenOnly] = useState(false);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setCount(
+      [party, breakfast, singleRoom, parking, swimming, womenOnly].filter(
+        v => v === true,
+      ).length,
+    );
+  }, [party, breakfast, singleRoom, parking, swimming, womenOnly]);
   return (
     <SafeAreaView>
       <StatusBar barStyle="default" />
       <View className="w-screen h-screen bg-white flex justify-center items-center">
         <View className="flex w-10/12 h-full">
-          <View className="flex w-full h-1/5  items-center justify-end">
-            <View className="flex flex-col w-full h-2/3">
-              <View className="flex flex-row w-full h-1/3 justify-between items-center">
+          <View className="flex w-full h-1/6  items-center justify-end">
+            <View className="flex flex-col w-full h-4/5">
+              <View className="flex flex-row w-full h-1/3 justify-between items-center ">
                 <Text className="font-inter-sb text-xl text-black">분위기</Text>
               </View>
               <View className="flex flex-row w-full h-1/3 justify-between">
-                <View className="flex flex-row w-1/2 h-full items-center justify-between px-2">
+                <View className="flex flex-row w-1/2 h-full items-center justify-between">
                   <Text className="font-inter-m text-md text-black ">
                     액티비티가 다양한
                   </Text>
-                  <CheckIcon width={19} height={19} />
+                  <Pressable
+                    className="flex w-auto h-auto rounded-md mx-1"
+                    onPress={() => setActivity(!activity)}
+                  >
+                    <CheckIcon
+                      width={19}
+                      height={19}
+                      stroke={`${activity ? '#39C3C5' : '#C2C2C2'}`}
+                    />
+                  </Pressable>
                 </View>
-                <View className="flex flex-row w-1/2 h-full items-center justify-between px-2">
+                <View className="flex flex-row w-1/2 h-full items-center justify-between">
                   <Text className="font-inter-m text-md text-black ">
                     힐링하기 좋은
                   </Text>
-                  <CheckIcon width={19} height={19} />
+                  <Pressable
+                    className="flex w-auto h-auto rounded-md mx-1"
+                    onPress={() => setHealing(!healing)}
+                  >
+                    <CheckIcon
+                      width={19}
+                      height={19}
+                      stroke={`${healing ? '#39C3C5' : '#C2C2C2'}`}
+                    />
+                  </Pressable>
                 </View>
               </View>
               <View className="flex flex-row w-full h-1/3 justify-between">
-                <View className="flex flex-row w-1/2 h-full items-center justify-between px-2">
+                <View className="flex flex-row w-1/2 h-full items-center justify-between">
                   <Text className="font-inter-m text-md text-black">
                     활기 넘치는
                   </Text>
-                  <CheckIcon width={19} height={19} />
+                  <Pressable
+                    className="flex w-auto h-auto rounded-md mx-1"
+                    onPress={() => setEnergetic(!energetic)}
+                  >
+                    <CheckIcon
+                      width={19}
+                      height={19}
+                      stroke={`${energetic ? '#39C3C5' : '#C2C2C2'}`}
+                    />
+                  </Pressable>
                 </View>
-                <View className="flex flex-row w-1/2 h-full items-center justify-between px-2">
+                <View className="flex flex-row w-1/2 h-full items-center justify-between">
                   <Text className="font-inter-m text-md text-black">
                     여유로운
                   </Text>
-                  <CheckIcon width={19} height={19} />
+                  <Pressable
+                    className="flex w-auto h-auto rounded-md mx-1"
+                    onPress={() => setLeisure(!leisure)}
+                  >
+                    <CheckIcon
+                      width={19}
+                      height={19}
+                      stroke={`${leisure ? '#39C3C5' : '#C2C2C2'}`}
+                    />
+                  </Pressable>
                 </View>
               </View>
             </View>
@@ -62,7 +118,9 @@ export default function FilterScreen({
                 시설 서비스
               </Text>
               <View className="flex flex-row w-auto h-auto items-center">
-                <Text className="font-inter-b text-md text-primary-2">2</Text>
+                <Text className="font-inter-b text-md text-primary-2">
+                  {count}
+                </Text>
                 <Text className="font-inter-b text-sm text-black/[.50]">
                   개 선택
                 </Text>
@@ -70,21 +128,56 @@ export default function FilterScreen({
             </View>
             <View className="flex flex-row w-full h-1/3 justify-center">
               <View className="flex flex-row w-10/12 h-full justify-between items-center">
-                <Pressable className="flex flex-col w-auto h-auto items-center">
-                  <PartyIcon width={37} height={44} />
-                  <Text className="font-inter-sb text-sm text-gray-3 py-1">
+                <Pressable
+                  className="flex flex-col w-auto h-auto items-center"
+                  onPress={() => setParty(!party)}
+                >
+                  <PartyIcon
+                    width={37}
+                    height={44}
+                    fill={`${party ? '#39C3C5' : '#C2C2C2'}`}
+                    stroke={`${party ? '#39C3C5' : '#C2C2C2'}`}
+                  />
+                  <Text
+                    className={`font-inter-sb text-sm text-${
+                      party ? 'primary-2' : 'gray-3'
+                    } py-1`}
+                  >
                     파티
                   </Text>
                 </Pressable>
-                <Pressable className="flex flex-col w-auto h-auto items-center">
-                  <BreakfastIcon width={42} height={44} />
-                  <Text className="font-inter-sb text-sm text-gray-3 py-1">
+                <Pressable
+                  className="flex flex-col w-auto h-auto items-center"
+                  onPress={() => setBreakfast(!breakfast)}
+                >
+                  <BreakfastIcon
+                    width={42}
+                    height={44}
+                    fill={`${breakfast ? '#39C3C5' : '#C2C2C2'}`}
+                    stroke={`${breakfast ? '#39C3C5' : '#C2C2C2'}`}
+                  />
+                  <Text
+                    className={`font-inter-sb text-sm text-${
+                      breakfast ? 'primary-2' : 'gray-3'
+                    } py-1`}
+                  >
                     조식
                   </Text>
                 </Pressable>
-                <Pressable className="flex flex-col w-auto h-auto items-center">
-                  <BedroomIcon width={44} height={44} />
-                  <Text className="font-inter-sb text-sm text-gray-3 py-1">
+                <Pressable
+                  className="flex flex-col w-auto h-auto items-center"
+                  onPress={() => setSingleRoom(!singleRoom)}
+                >
+                  <BedroomIcon
+                    width={44}
+                    height={44}
+                    fill={`${singleRoom ? '#39C3C5' : '#C2C2C2'}`}
+                  />
+                  <Text
+                    className={`font-inter-sb text-sm text-${
+                      singleRoom ? 'primary-2' : 'gray-3'
+                    } py-1`}
+                  >
                     1인실
                   </Text>
                 </Pressable>
@@ -92,21 +185,54 @@ export default function FilterScreen({
             </View>
             <View className="flex w-full h-1/3 items-center">
               <View className="flex flex-row w-10/12 h-full justify-between items-center">
-                <Pressable className="flex flex-col w-auto h-auto items-center">
-                  <ParkingIcon width={50} height={44} />
-                  <Text className="font-inter-sb text-sm text-gray-3 py-1">
-                    파티
+                <Pressable
+                  className="flex flex-col w-auto h-auto items-center"
+                  onPress={() => setParking(!parking)}
+                >
+                  <ParkingIcon
+                    width={50}
+                    height={44}
+                    fill={`${parking ? '#39C3C5' : '#C2C2C2'}`}
+                  />
+                  <Text
+                    className={`font-inter-sb text-sm text-${
+                      parking ? 'primary-2' : 'gray-3'
+                    } py-1`}
+                  >
+                    주차
                   </Text>
                 </Pressable>
-                <Pressable className="flex flex-col w-auto h-auto items-center">
-                  <SwimmingIcon width={48} height={44} />
-                  <Text className="font-inter-sb text-sm text-gray-3 py-1">
+                <Pressable
+                  className="flex flex-col w-auto h-auto items-center"
+                  onPress={() => setSwimming(!swimming)}
+                >
+                  <SwimmingIcon
+                    width={48}
+                    height={44}
+                    stroke={`${swimming ? '#39C3C5' : '#C2C2C2'}`}
+                  />
+                  <Text
+                    className={`font-inter-sb text-sm text-${
+                      swimming ? 'primary-2' : 'gray-3'
+                    } py-1`}
+                  >
                     수영
                   </Text>
                 </Pressable>
-                <Pressable className="flex flex-col w-auto h-auto items-center">
-                  <WomanIcon width={46} height={44} />
-                  <Text className="font-inter-sb text-sm text-gray-3 py-1">
+                <Pressable
+                  className="flex flex-col w-auto h-auto items-center"
+                  onPress={() => setWomenOnly(!womenOnly)}
+                >
+                  <WomanIcon
+                    width={46}
+                    height={44}
+                    stroke={`${womenOnly ? '#39C3C5' : '#C2C2C2'}`}
+                  />
+                  <Text
+                    className={`font-inter-sb text-sm text-${
+                      womenOnly ? 'primary-2' : 'gray-3'
+                    } py-1`}
+                  >
                     여성전용
                   </Text>
                 </Pressable>
@@ -117,8 +243,7 @@ export default function FilterScreen({
             <View className="flex w-full h-auto">
               <Text className="font-inter-sb text-xl text-black">가격</Text>
             </View>
-            <View className="flex w-full h-2/5 bg-blue-500">
-            </View>
+            <View className="flex w-full h-2/5 bg-blue-500"></View>
           </View>
         </View>
       </View>
