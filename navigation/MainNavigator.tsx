13b8b8be73bb4620d5e754b275cafef3b3970d21
minjung <k1950035@naver.com>
2024-06-05 02:57:13 +0900

    Feat: Bottom navigation
    하단의 바텀 네비게이션을 구현했습니다. (실질적으로 구현되지 않을 채팅 페이지는 제외하였습니다)

--- navigation/MainNavigator.tsx
+++ navigation/MainNavigator.tsx
@@ -3,11 +3,29 @@ import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import RecommendationsScreen from '../screens/main/RecommendationsScreen';
 import SearchNavigator from './SearchNavigator';
-import ChatNavigator from './ChatNavigator';
 import MyPageNavigator from './MyPageNavigator';
 import FavoritesScreen from '../screens/main/FavoritesScreen';
-import GuesthouseDetailsNavigator from './GuesthouseDetailsNavigator';
 import { MainTabParamList } from './types';
+import IconHeart from '../assets/images/icon_heartnavigator.svg';
+import IconProfile from '../assets/images/icon_profilenavigator.svg';
+import IconHome from '../assets/images/icon_homenavigator.svg';
+import IconSearch from '../assets/images/icon_serachnavigator.svg';
+
+const BottomIconHeart = ({ focused }: { focused: boolean }) => (
+  <IconHome width={24} height={24} fill={focused ? '#000000' : '#C2C2C2'} />
+);
+
+const BottomIconSearch = ({ focused }: { focused: boolean }) => (
+  <IconSearch width={24} height={24} fill={focused ? '#000000' : '#C2C2C2'} />
+);
+
+const BottomIconProfile = ({ focused }: { focused: boolean }) => (
+  <IconProfile width={24} height={24} fill={focused ? '#000000' : '#C2C2C2'} />
+);
+
+const BottomIconFavorites = ({ focused }: { focused: boolean }) => (
+  <IconHeart width={24} height={24} fill={focused ? '#000000' : '#C2C2C2'} />
+);
 
 export default function MainNavigator() {
   const MainTab = createBottomTabNavigator<MainTabParamList>();
@@ -20,11 +38,18 @@ export default function MainNavigator() {
           display:
             route.name === 'GuesthouseDetailsNavigator' ? 'none' : 'flex',
         },
+        tabBarActiveTintColor: '#000000',
+        tabBarInactiveTintColor: '#C2C2C2',
+        tabBarShowLabel: true,
       })}
     >
       <MainTab.Screen
         name="Recommendations"
         component={RecommendationsScreen}
+        options={{
+          title: '홈',
+          tabBarIcon: focused => BottomIconHeart(focused),
+        }}
       />
       <MainTab.Screen
         name="SearchNavigator"
@@ -39,19 +64,26 @@ export default function MainNavigator() {
                 : 'flex',
           },
           headerShown: false,
+          title: '검색',
+          tabBarIcon: ({ focused }) => BottomIconSearch({ focused }),
         })}
       />
-      <MainTab.Screen name="ChatNavigator" component={ChatNavigator} />
+      <MainTab.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          title: '찜',
+          tabBarIcon: ({ focused }) => BottomIconFavorites({ focused }),
+        }}
+      />
       <MainTab.Screen
         name="MyPageNavigator"
         component={MyPageNavigator}
-        options={{ headerShown: false }}
-      />
-      <MainTab.Screen name="Favorites" component={FavoritesScreen} />
-      <MainTab.Screen
-        name="GuesthouseDetailsNavigator"
-        component={GuesthouseDetailsNavigator}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          title: '내 정보',
+          tabBarIcon: ({ focused }) => BottomIconProfile({ focused }),
+        }}
       />
     </MainTab.Navigator>
   );
