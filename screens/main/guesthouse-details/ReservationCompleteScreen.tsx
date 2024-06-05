a4b03ddf160c121f0a602d60319f76ca07d7a1e4
leeji0805 <bomnaeum104@naver.com>
2024-06-05 19:09:11 +0900

    Feat: 예약 완료 페이지 구현
    + 더미 이미지 추가

--- screens/main/guesthouse-details/ReservationCompleteScreen.tsx
+++ screens/main/guesthouse-details/ReservationCompleteScreen.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
-import { SafeAreaView, StatusBar, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  View,
+  Text,
+  Pressable,
+  Image,
+} from 'react-native';
 import { GuesthouseDetailsStackScreenProps } from '../../../navigation/types';
+import LocalSvg from '../../../assets/images/icon_local.svg';
+import PhoneSVG from '../../../assets/images/icon_phone.svg';
+import { ReservationRecord } from '../../../types';
 
+import dummyImage from '../../../assets/images/dummy_img';
+
+const reservation: ReservationRecord = {
+  guesthouse_name: '서점 숙소',
+  guesthouse_address: '제주시 조천읍 북촌리 283',
+  guesthouse_phone: '010-1234-5678',
+  checkin_date: '2024-03-30T15:00:00',
+  checkout_date: '2024-03-31T11:00:00',
+  nights: 1,
+};
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    weekday: 'short',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+};
 export default function ReservationCompleteScreen({
   // route와 navigation 사용 안할 시 제거해주세요.
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -9,11 +41,93 @@ export default function ReservationCompleteScreen({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   navigation,
 }: GuesthouseDetailsStackScreenProps<'ReservationComplete'>) {
+  const [imageError, setImageError] = useState(false);
   return (
     <SafeAreaView>
-      <StatusBar barStyle="default" />
-      <View className="h-screen bg-slate-500 flex justify-center items-center">
-        <Text className="text-5xl text-white">ReservationCompleteScreen</Text>
+      <StatusBar barStyle="dark-content" />
+      <View className=" h-screen bg-gray-1 gap-2 flex justify-start items-center">
+        <View className="flex justify-start h-3/5 w-full bg-white items-center  py-4 px-4">
+          <Text className="font-inter-b w-full text-left text-xl2 mt-4 text-black">
+            {reservation.guesthouse_name}
+          </Text>
+          <Text className="font-inter-b  w-full text-left text-xl2 text-black">
+            예약이 완료되었습니다.
+          </Text>
+          <View className="flex-row h-3/5 w-full justify-center items-center mt-4 rounded-lg mb-4">
+            {dummyImage && !imageError ? (
+              <Image
+                source={{ uri: dummyImage }}
+                className="w-full h-full rounded-lg"
+                onError={() => setImageError(true)}
+              />
+            ) : (
+              <Text className="text-white font-inter-b text-lg">
+                이미지 로드를 실패했습니다.
+              </Text>
+            )}
+          </View>
+          <View className="flex-row justify-center items-center w-full h-1/6 bg-white rounded-lg">
+            <View className="flex-row justify-center items-center w-[86%] h-full bg-gray-1 rounded-lg ">
+              <View className="flex-row absolute h-[78.38%] w-[0.58%] bg-gray-3" />
+              <View className="fflex w-[50%] h-full justify-nomal items-start p-2  rounded-l-lg">
+                <Text
+                  className=" font-inter-m text-sss text-black flex-shrink"
+                  numberOfLines={1}
+                >
+                  Check In
+                </Text>
+                <Text
+                  className="font-inter-m text-ss text-black flex-shrink"
+                  numberOfLines={2}
+                >
+                  {formatDate(reservation.checkin_date)}
+                </Text>
+              </View>
+              <View className="flex w-[50%] h-full justify-nomal items-start p-2 ">
+                <Text
+                  className=" font-inter-m text-sss text-black flex-shrink"
+                  numberOfLines={1}
+                >
+                  Check Out
+                </Text>
+                <Text
+                  className="mr-1 font-inter-m text-ss text-black flex-shrink"
+                  numberOfLines={2}
+                >
+                  {formatDate(reservation.checkout_date)}
+                </Text>
+              </View>
+            </View>
+            <View className="flex justify-center items-center w-[14%] h-full bg-primary-2 rounded-r-lg">
+              <Text className=" font-inter-sb text-lg text-white">
+                {reservation.nights}박
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View className="flex justify-start h-2/5 w-full bg-white items-center py-4">
+          <View className="flex justify-start items-center h-3/5 w-full px-4 py-4">
+            <Text className="w-full text-left font-inter-b text-md text-black mb-4">
+              숙소 정보
+            </Text>
+            <View className="flex-row w-full h-1/6 justify-start items-center mt-2">
+              <LocalSvg height="95%" width="7%" />
+              <Text className="ml-2 font-inter-r text-sm text-black">
+                {reservation.guesthouse_address}
+              </Text>
+            </View>
+            <View className="flex-row w-full h-1/6 justify-start items-center mt-2">
+              <PhoneSVG height="95%" width="7%" />
+              <Text className="ml-2 font-inter-r text-sm text-black">
+                {reservation.guesthouse_phone}
+              </Text>
+            </View>
+          </View>
+          <Pressable className="my-4 bg-primary-2 items-center justify-center w-full h-1/5 shadow-black drop-shadow-xl">
+            <Text className="font-inter-b text-lg text-white">예약하기</Text>
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
