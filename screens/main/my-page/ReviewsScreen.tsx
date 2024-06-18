5f9b6504a832e0f3bcba0a6159850576a20e1e9a
leeji0805 <bomnaeum104@naver.com>
2024-06-18 18:42:03 +0900

    Feat: 리뷰 작성 페이지에서 게하 리뷰, 게스트 리뷰 헨들링 추가

--- screens/main/my-page/ReviewsScreen.tsx
+++ screens/main/my-page/ReviewsScreen.tsx
@@ -11,16 +11,13 @@ import {
 import { MyPageStackScreenProps } from '../../../navigation/types';
 import RatingStars from '../../../components/common/RatingStars';
 import GuestReviewCard from '../../../components/my-page/review-page/GuestReviewCard';
-import { useAppDispatch, useAppSelector } from '../../../api/hooks';
-import { setGuesthouseId } from '../../../api/slices/guesthouseSlice';
+import { useAppSelector, useRegisterSticker } from '../../../api/hooks';
 import { useGetReservationStatusQuery } from '../../../api/endpoints/reservationEndpoints';
 import { useGetHangoutUserQuery } from '../../../api/endpoints/guestEndpoints';
+import { useRegisterStickerMutation } from '../../../api/endpoints/userEndpoints';
 
 export default function ReviewsScreen({
-  // route와 navigation 사용 안할 시 제거해주세요.
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   route,
-
   navigation,
 }: MyPageStackScreenProps<'Reviews'>) {
   const screenHeight = Dimensions.get('window').height;
@@ -31,13 +28,46 @@ export default function ReviewsScreen({
   const selectedReservation = reservationData?.reservationRecords.find(
     record => record.guesthouseId === guesthouseIdState
   );
-  const dispatch = useAppDispatch();
-  const guesthouseData = useGetHangoutUserQuery(selectedReservation?.reservationId ?? 0).data
+  const guesthouseData = useGetHangoutUserQuery(selectedReservation?.reservationId ?? 0).data;
   const guests = useGetHangoutUserQuery(selectedReservation?.reservationId ?? 0).data?.guests ?? [];
+  
   const [rating, setRating] = useState(0);
+  const [selectedReviews, setSelectedReviews] = useState<{ [key: string]: string[] }>({});
+
   const handleRatingChange = (newRating: number) => {
     setRating(newRating);
     console.log(`Updated rating: ${newRating}`);
+  };
+
+  const handleReviewChange = (nickname: string, reviews: string[]) => {
+    setSelectedReviews(prevState => ({
+      ...prevState,
+      [nickname]: reviews,
+    }));
+  };
+
+  const registerSticker = useRegisterSticker();
+
+  const handleSubmit = async () => {
+    for (const guest of guests) {
+      const stickerTexts = selectedReviews[guest.nickname];
+      
+      if (stickerTexts && stickerTexts.length > 0) {
+        try {
+          await registerSticker.handleRegistSticker([
+            {
+              userId: guest.userId,
+              stickerTexts,
+            },
+          ]);
+          console.log(`Successfully registered stickers for ${guest.nickname}, ${guest.userId} with texts: ${stickerTexts}`);
+        } catch (error) {
+          console.error(`Failed to register sticker for ${guest.nickname}`, error);
+        }
+      }
+    }
+    console.log('Successfully registered stickers');
+    navigation.navigate('MyPage');
   };
 
   return (
@@ -67,7 +97,7 @@ export default function ReviewsScreen({
             <Text className="my-5 text-md font-inter-sb text-black">
               게스트 하우스는 어떠셨나요?
             </Text>
-            <RatingStars  onRatingChange={handleRatingChange}/>
+            <RatingStars onRatingChange={handleRatingChange} />
           </View>
           <View className="px-4 w-full my-2">
             <Text className=" text-left text-md font-inter-sb text-black">
@@ -75,22 +105,17 @@ export default function ReviewsScreen({
             </Text>
           </View>
           {guests.map(guest => (
-            <>
-              <GuestReviewCard
-                key={guest.nickname}
-                name={guest.nickname}
-                mbti={guest.mbti}
-              />
-              <View
-                className="bg-white h-[2%] w-full"
-                style={{ height: (screenHeight * 1) / 14 }}
-              />
-            </>
+            <GuestReviewCard
+              key={guest.nickname}
+              name={guest.nickname}
+              mbti={guest.mbti}
+              onReviewChange={handleReviewChange}
+            />
           ))}
           <Pressable
             className="bg-primary-2 items-center justify-center w-full"
             style={{ height: (screenHeight * 1) / 14 }}
-            onPress={() => navigation.navigate('MyPage')}
+            onPress={handleSubmit}
           >
             <Text className="font-inter-b text-lg text-white">등록하기</Text>
           </Pressable>
